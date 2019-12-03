5faf867741763aa6b332acb0f20c67b31ecf2077
Sara Gladchun <45917390+skglad@users.noreply.github.com>
2019-12-03 10:28:58 -0500

    Delete instav2.js

--- static/instav2.js
+++
@@ -1,1 +0,0 @@
-fetch('https://api.nytimes.com/svc/books/v3/lists.json?list-name=hardcover-fiction&api-key=' + apiKey, {    method: 'get',  })  .then(response => { return response.json(); })  .then(json => { console.log(json); });
